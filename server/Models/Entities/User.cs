ed8fba9cb7452b2efa2ea68972c6cc173d5f6819
Faiz Khalifah <faiz_doc@outlook.com>
2025-02-09 21:57:37 +0700

    feat: user model

---
+++ server/Models/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace server.Models.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }= Guid.NewGuid();
+        public string Username { get; set; }
+        public string HashedPassword { get; set; }
+
+        public string Role { get; set; } = "User";
+    }
+}
